bb8a89ab3dc84ce34e39cf097fea9bba7a61056d
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2019-02-07 23:50:09 +1000

    updates
    styles, js time render, time hover display

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
+import moment from 'moment'
 
+// clock element DOM elements defined 
+const getTime = moment().format("LT") 
+    
+const clockEl = document.querySelector("#clock")
+const timeDivEl = document.createElement('div')
+timeDivEl.classList.add('time-div-element')
 
-// create DOM for time display
+const timeSpanEl = document.createElement('span')
+timeSpanEl.classList.add('time-span-element')
 
+const timeElPara = document.createElement('p')
+timeElPara.classList.add('time-element-parargraph')
+timeElPara.textContent = getTime
+
+// render time display DOM 
 
 const renderTimeDOM = function () {
-    const now = new Date()
-    const getTime = now.toLocaleTimeString()
-    
-    const clockEl = document.querySelector("#clock")
-    const timeDivEl = document.createElement('div')
-    timeDivEl.classList.add('time-div-element')
-
-    const timeSpanEl = document.createElement('span')
-    timeSpanEl.classList.add('time-span-element')
-    
-    const timeElPara = document.createElement('p')
-    timeElPara.classList.add('time-element-parargraph')
-    timeElPara.textContent = getTime
-
+    //const now = new Date()
+    //now.toLocaleTimeString()
     clockEl.innerHTML = ""
-    
     timeSpanEl.appendChild(timeElPara)
     timeDivEl.appendChild(timeSpanEl)
     clockEl.appendChild(timeDivEl)
-
 }
 setInterval(renderTimeDOM(), 1000)
+
+
+
 
