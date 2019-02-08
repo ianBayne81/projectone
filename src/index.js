589029e472b28fe122f1d062b6def8872666752e
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2019-02-08 23:50:54 +1000

    updates
    css checkbox curtain

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import moment from 'moment'
 // clock element DOM elements defined 
 const getTime = moment().format("LT") 
     
-const clockEl = document.querySelector("#clock")
-const timeDivEl = document.createElement('div')
-timeDivEl.classList.add('time-div-element')
-
-const timeSpanEl = document.createElement('span')
-timeSpanEl.classList.add('time-span-element')
-
-const timeElPara = document.createElement('p')
-timeElPara.classList.add('time-element-parargraph')
-timeElPara.textContent = getTime
+    const clockEl = document.querySelector("#clock")
+    const timeDivEl = document.createElement('div')
+    timeDivEl.classList.add('time-div-element')
+    
+    const timeSpanEl = document.createElement('span')
+    timeSpanEl.classList.add('time-span-element')
+    
+    const timeElPara = document.createElement('p')
+    timeElPara.classList.add('time-element-parargraph')
+    timeElPara.textContent = getTime
 
 // render time display DOM 
 
